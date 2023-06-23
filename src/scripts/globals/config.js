efb79ef493ef298aa0d660d0e1e966b62b2744e2
Ardi Syahputra <ardisaputraa12@gmail.com>
2023-06-23 23:58:48 +0700

    create request from API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'd32c9ac6accdee12edce1834136446f7',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
