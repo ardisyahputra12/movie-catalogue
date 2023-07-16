31175e578f013d323f6f0701fea139fc8d23ed68
Ardi Syahputra <ardisaputraa12@gmail.com>
2023-07-17 00:12:36 +0700

    create end to end testing

---
+++ e2e/Liking_Movies.spec.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+Feature('Liking Movies');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
+
+Scenario('liking one movie', async ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.movie-item');
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
+});
